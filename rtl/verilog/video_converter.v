fae888263ad98b2421b03052e2a49722a2133a5c
creep <creep@42a503f1-3539-4ca3-bca3-ad517a917e51>
2009-05-08 20:28:03 +0000

    Video YPbPr to RGB is coded.

--- rtl/verilog/video_converter.v
+++ rtl/verilog/video_converter.v
@@ -44,12 +44,14 @@
 
 
 module video_converter(ypbpr, rgb);
-	input [6:0] ypbpr;
-	output [23:0] rgb;
 
-	case (ypbpr[2:0]) begin // luminance
+input [6:0] ypbpr;
+output reg [23:0] rgb;
+
+always @(*) begin
+	case (ypbpr[2:0]) // luminance
 		3'h0: begin 
-			case (ypbpr[6:3]) begin
+			case (ypbpr[6:3])
 				4'h0: rgb = 24'h000000;
 				4'h1: rgb = 24'h444400;
 				4'h2: rgb = 24'h702800;
@@ -63,19 +65,152 @@ module video_converter(ypbpr, rgb);
 				4'hA: rgb = 24'h002C5C;
 				4'hB: rgb = 24'h003C2C;
 				4'hC: rgb = 24'h003C00;
-				4'hD: rgb = 24'h;
-				4'hE: rgb = 24'h;
-				4'hF: rgb = 24'h;
+				4'hD: rgb = 24'h143800;
+				4'hE: rgb = 24'h2C3000;
+				4'hF: rgb = 24'h442800;
 			endcase
 		end
-		3'h1: 
-		3'h2: 
-		3'h3: 
-		3'h4: 
-		3'h5: 
-		3'h6: 
-		3'h7: 
+		3'h1: begin 
+			case (ypbpr[6:3])
+				4'h0: rgb = 24'h404040;
+				4'h1: rgb = 24'h646410;
+				4'h2: rgb = 24'h844414;
+				4'h3: rgb = 24'h983418;
+				4'h4: rgb = 24'h9C2020;
+				4'h5: rgb = 24'h8C2074;
+				4'h6: rgb = 24'h602090;
+				4'h7: rgb = 24'h302098;
+				4'h8: rgb = 24'h1C209C;
+				4'h9: rgb = 24'h1C3890;
+				4'hA: rgb = 24'h1C4C78;
+				4'hB: rgb = 24'h1C5C48;
+				4'hC: rgb = 24'h205C20;
+				4'hD: rgb = 24'h345C1C;
+				4'hE: rgb = 24'h4C501C;
+				4'hF: rgb = 24'h644818;
+			endcase
+		end
+		3'h2: begin 
+			case (ypbpr[6:3])
+				4'h0: rgb = 24'h6C6C6C;
+				4'h1: rgb = 24'h848424;
+				4'h2: rgb = 24'h985C28;
+				4'h3: rgb = 24'hAC5030;
+				4'h4: rgb = 24'hB03C3C;
+				4'h5: rgb = 24'hA03C88;
+				4'h6: rgb = 24'h783CA4;
+				4'h7: rgb = 24'h4C3CAC;
+				4'h8: rgb = 24'h3840B0;
+				4'h9: rgb = 24'h3854A8;
+				4'hA: rgb = 24'h386890;
+				4'hB: rgb = 24'h387C64;
+				4'hC: rgb = 24'h407C40;
+				4'hD: rgb = 24'h507C38;
+				4'hE: rgb = 24'h687034;
+				4'hF: rgb = 24'h846830;
+			endcase
+		end
+		3'h3: begin 
+			case (ypbpr[6:3])
+				4'h0: rgb = 24'h909090;
+				4'h1: rgb = 24'hA0A034;
+				4'h2: rgb = 24'hAC783C;
+				4'h3: rgb = 24'hC06848;
+				4'h4: rgb = 24'hC05858;
+				4'h5: rgb = 24'hB0589C;
+				4'h6: rgb = 24'h8C58B8;
+				4'h7: rgb = 24'h6858C0;
+				4'h8: rgb = 24'h505CC0;
+				4'h9: rgb = 24'h5070BC;
+				4'hA: rgb = 24'h5084AC;
+				4'hB: rgb = 24'h509C80;
+				4'hC: rgb = 24'h5C9C5C;
+				4'hD: rgb = 24'h6C9850;
+				4'hE: rgb = 24'h848C4C;
+				4'hF: rgb = 24'hA08444;
+			endcase
+		end
+		3'h4: begin 
+			case (ypbpr[6:3])
+				4'h0: rgb = 24'hB0B0B0;
+				4'h1: rgb = 24'hB8B840;
+				4'h2: rgb = 24'hBC8C4C;
+				4'h3: rgb = 24'hD0805C;
+				4'h4: rgb = 24'hD07070;
+				4'h5: rgb = 24'hC070B0;
+				4'h6: rgb = 24'hA070CC;
+				4'h7: rgb = 24'h7C70D0;
+				4'h8: rgb = 24'h6874D0;
+				4'h9: rgb = 24'h6888CC;
+				4'hA: rgb = 24'h689CC0;
+				4'hB: rgb = 24'h68B494;
+				4'hC: rgb = 24'h74B474;
+				4'hD: rgb = 24'h84B468;
+				4'hE: rgb = 24'h9CA864;
+				4'hF: rgb = 24'hB89C58;
+			endcase
+		end
+		3'h5: begin 
+			case (ypbpr[6:3])
+				4'h0: rgb = 24'hC8C8C8;
+				4'h1: rgb = 24'hD0D050;
+				4'h2: rgb = 24'hCCA05C;
+				4'h3: rgb = 24'hE09470;
+				4'h4: rgb = 24'hE08888;
+				4'h5: rgb = 24'hD084C0;
+				4'h6: rgb = 24'hB484DC;
+				4'h7: rgb = 24'h9488E0;
+				4'h8: rgb = 24'h7C8CE0;
+				4'h9: rgb = 24'h7C9CDC;
+				4'hA: rgb = 24'h7CB4D4;
+				4'hB: rgb = 24'h7CD0AC;
+				4'hC: rgb = 24'h8CD08C;
+				4'hD: rgb = 24'h9CCC7C;
+				4'hE: rgb = 24'hB4C078;
+				4'hF: rgb = 24'hD0B46C;
+			endcase
+		end
+		3'h6: begin 
+			case (ypbpr[6:3])
+				4'h0: rgb = 24'hDCDCDC;
+				4'h1: rgb = 24'hE8E85C;
+				4'h2: rgb = 24'hDCB468;
+				4'h3: rgb = 24'hECA880;
+				4'h4: rgb = 24'hECA0A0;
+				4'h5: rgb = 24'hDC9CD0;
+				4'h6: rgb = 24'hC49CEC;
+				4'h7: rgb = 24'hA8A0EC;
+				4'h8: rgb = 24'h90A4EC;
+				4'h9: rgb = 24'h90B4EC;
+				4'hA: rgb = 24'h90CCE8;
+				4'hB: rgb = 24'h90E4C0;
+				4'hC: rgb = 24'hA4E4A4;
+				4'hD: rgb = 24'hB4E490;
+				4'hE: rgb = 24'hCCD488;
+				4'hF: rgb = 24'hE8CC7C;
+			endcase
+		end
+		3'h7: begin 
+			case (ypbpr[6:3])
+				4'h0: rgb = 24'hECECEC;
+				4'h1: rgb = 24'hFCFC68;
+				4'h2: rgb = 24'hECC878;
+				4'h3: rgb = 24'hFCBC94;
+				4'h4: rgb = 24'hFCB4B4;
+				4'h5: rgb = 24'hECB0E0;
+				4'h6: rgb = 24'hD4B0FC;
+				4'h7: rgb = 24'hBCB4FC;
+				4'h8: rgb = 24'hA4B8FC;
+				4'h9: rgb = 24'hA4C8FC;
+				4'hA: rgb = 24'hA4E0FC;
+				4'hB: rgb = 24'hA4FCD4;
+				4'hC: rgb = 24'hB8FCB8;
+				4'hD: rgb = 24'hC8FCA4;
+				4'hE: rgb = 24'hE0EC9C;
+				4'hF: rgb = 24'hFCE08C;
+			endcase
+		end
 	endcase
-
+end
 endmodule
 
